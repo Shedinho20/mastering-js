024d79ff1ae3cb04ddf027015336d4c79e05553e
shedinho20 <shedinhoshedrach@gmail.com>
2020-02-20 21:46:50 +0300

    completed first task

--- src/arrays/iteration.js
+++ src/arrays/iteration.js
@@ -9,17 +9,26 @@
  */
 function solution(A) {
   // todo: implement function here
-  return 0
+  var i;
+  let count = 0;
+  for (i = 0; i < A.length; i++){
+    if (A[i] % 2 === 0){
+      count=count+1
+    }
+  }
+  return count;
 }
+
+
 
 function main() {
   const A = new Array(125).fill(0)
     .map((_, i) => i)
 
   const y = solution(A)
-
   if (y !== 63) console.error("Wrong Answer: Please implement a correct solution")
   else console.log("Correct, Welldone")
 }
 
 main()
+
