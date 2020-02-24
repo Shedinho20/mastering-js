2a88005f6632d7616daf4dcaa602e91dc1828b2f
King Elisha <ek.chibueze@gmail.com>
2020-02-24 06:45:51 +0100

    setup basic test

--- src/arrays/counting.js
+++ src/arrays/counting.js
@@ -28,27 +28,40 @@ function solution1(A) {
 
 /**
  * TASK 2: Given an array of integers from [0, N], write a function
- * that returns the smallest missing integer
+ * that returns the smallest missing integer. For example, given an array:
+ * A = [1,3,2,5,6,-1,0,8], the function should return 4
  */
 
 /**
  * Finds the smallest missing integer
  * @param {number[]} A
  */
-function solution1(A) {
+function solution2(A) {
   // todo: implement function here
+  return 0;
 }
 
 function main() {
-  const A = new Array(125).fill(0).map((_, i) => i + 1);
+  const A1 = new Array(125).fill(0).map((_, i) => i + 1);
 
-  const y = solution1(A);
+  const y1 = solution1(A1);
   if (
-    y.join() !==
+    y1.join() !==
     [7, 15, 23, 31, 39, 47, 55, 63, 71, 79, 87, 95, 103, 111, 119].join()
   )
-    console.error("Wrong Answer: Please implement a correct solution");
-  else console.log("Correct, Welldone");
+    console.error(
+      "Solution1 Wrong Answer: Please implement a correct solution"
+    );
+  else console.log("Solution1 Correct, Welldone");
+
+  const A2 = [2, -1, 0, 1, 4, 3];
+
+  const y2 = solution2(A2);
+  if (y2 !== 5)
+    console.error(
+      "Solution2 Wrong Answer: Please implement a correct solution"
+    );
+  else console.log("Solution2 Correct, Welldone");
 }
 
 main();
