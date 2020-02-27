4d980e8abd63e28dbe367e7d021c15d0026c2c9b
King Elisha <ek.chibueze@gmail.com>
2020-02-27 09:50:08 +0100

    new task

---
+++ src/functionsAndVariables/reverse.js
@@ -0,0 +1,18 @@
+/**
+ * TASK 1: Write a function to reverse the number N
+ * example: 34221 ---> 12243
+ * @param {number} N
+ */
+function solution1(N) {
+  //todo: implement solution here
+  return 0;
+}
+
+function main() {
+  const N1 = 10293848493;
+
+  const y1 = solution1(N1);
+  if (y1 !== 39484839201)
+    console.error("Wrong Answer: Please implement a correct solution");
+  else console.log("Correct, Welldone");
+}
